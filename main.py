185a0ec44ab064c32f41429ae1bc9b3091c91e40
igorcrexito <igorcrexito@gmail.com>
2025-01-13 22:32:41 -0300

    changing model

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from model_architectures.single_model import SingleModel
 from video_utils import video
 import os
+import numpy as np
 
 DATASET_PATH = '../faceforensics/'
 DESCRIPTOR_LIST = ['gabor', 'lbp', 'halo', 'fourier']
@@ -9,11 +11,37 @@ if __name__ == '__main__':
     ## retrieving the list of all dataset files
     file_paths = os.listdir(DATASET_PATH)
 
-    ## iterating over dataset files
-    for video_file_path in file_paths:
+    ## instantiating an array to store video information
+    input_head = np.ones((len(file_paths), 32, 80, 80, 7))
+    input_forehead = np.ones((len(file_paths), 32, 32, 32, 7))
+    input_left_cheek = np.ones((len(file_paths), 32, 24, 24, 7))
+    input_right_cheek = np.ones((len(file_paths), 32, 24, 24, 7))
+    input_mouth = np.ones((len(file_paths), 32, 20, 20, 7))
+
+    ## iterating over dataset files and creating data structures
+    for index, video_file_path in enumerate(file_paths):
         video_regions = video.Video(video_path=f'{DATASET_PATH}{video_file_path}', descriptor_list=DESCRIPTOR_LIST,
                                     number_of_sampled_frames = 32, frame_width=480, frame_height=480)
 
-        a, b, c, d, e = video_regions.retrieve_face_areas()
+        ## composing model outputs
+        face, forehead, left_cheek, right_cheek, mouth = video_regions.retrieve_face_areas()
+        input_head[index] = face
+        input_forehead[index] = forehead
+        input_left_cheek[index] = left_cheek
+        input_right_cheek[index] = right_cheek
+        input_mouth[index] = mouth
 
-    __import__("IPython").embed()
+
+    ## instantiating the classification model
+    model = SingleModel(summarize_model=True)
+
+    labels = np.array([[0, 1], [0, 1], [1, 0]])
+
+    ## training the model
+    model.fit_model(input_data_head=input_head,
+                    input_data_forehead=input_forehead,
+                    input_data_right_cheek=input_right_cheek,
+                    input_data_left_cheek=input_left_cheek,
+                    input_data_mouth=input_mouth,
+                    labels=labels,
+                    number_of_epochs=5)
---
+++ model_architectures/single_model.py
@@ -0,0 +1,114 @@
+from keras.layers import Input, Conv2D, Conv3D, MaxPooling2D, UpSampling2D, Concatenate, LayerNormalization, ZeroPadding2D, \
+    BatchNormalization, Flatten, Dense, Reshape, DepthwiseConv2D, Add, Dropout, MultiHeadAttention, Rescaling, Activation
+from keras.models import Model
+import numpy as np
+import tensorflow as tf
+import keras
+from tensorflow.keras.optimizers import Adam
+import sys
+sys.path.insert(0, '..')
+
+class SingleModel:
+
+    def __init__(self, summarize_model: bool, pre_trained_path: str = None):
+        self.summarize_model = summarize_model
+        if pre_trained_path is not None:
+            self.model = keras.models.load_model(pre_trained_path)
+        else:
+            self.model = self._create_model()
+
+
+    def _create_model(self):
+        # Number of inputs
+        num_inputs = 5
+        inputs = []
+        processed_inputs = []
+
+        # Define Conv3D and Flatten layers for each input
+        for i in range(num_inputs):
+            if i == 0:
+                input_layer = Input(shape=(32, 80, 80, 7), name=f'input_face')
+                inputs.append(input_layer)
+            elif i == 1:
+                input_layer = Input(shape=(32, 32, 32, 7), name=f'input_forehead')
+                inputs.append(input_layer)
+            elif i == 2:
+                input_layer = Input(shape=(32, 24, 24, 7), name=f'input_left_cheek')
+                inputs.append(input_layer)
+            elif i == 3:
+                input_layer = Input(shape=(32, 24, 24, 7), name=f'input_right_cheek')
+                inputs.append(input_layer)
+            else:
+                input_layer = Input(shape=(32, 20, 20, 7), name=f'input_mouth')
+                inputs.append(input_layer)
+
+            # Conv3D block
+            x = Conv3D(filters=16, kernel_size=(3, 3, 3), activation='relu', padding='same')(input_layer)
+            x = Conv3D(filters=16, kernel_size=(3, 3, 3), activation='relu', padding='same')(x)
+            x = Conv3D(filters=32, kernel_size=(3, 3, 3), activation='relu', padding='same')(x)
+
+            # Flatten the output
+            x = Flatten()(x)
+            processed_inputs.append(x)
+
+        # Concatenate the flattened outputs from all branches
+        concatenated = Concatenate()(processed_inputs)
+
+        # Fully connected layers
+        x = Dense(units=64, activation='relu')(concatenated)
+        x = Dropout(0.5)(x)
+        x = Dense(units=32, activation='relu')(x)
+        x = Dropout(0.5)(x)
+        x = Dense(units=16, activation='relu')(x)
+
+        # Output layer for binary classification
+        output = Dense(units=2, activation='softmax', name='output')(x)
+
+        # Create the model
+        model = Model(inputs=inputs, outputs=output)
+
+        # Compile the model
+        model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=['accuracy'])
+
+        # Summarize the model if needed
+        if self.summarize_model:
+            model.summary()
+
+        return model
+
+    def fit_model(self, input_data_head: np.ndarray, input_data_forehead: np.ndarray, input_data_left_cheek: np.ndarray,
+                  input_data_right_cheek: np.ndarray, input_data_mouth: np.ndarray, labels: np.ndarray, number_of_epochs: int):
+        """
+        Fit the model with multiple inputs.
+
+        Args:
+            input_data_head (np.ndarray): Input data for the head region.
+            input_data_forehead (np.ndarray): Input data for the forehead region.
+            input_data_left_cheek (np.ndarray): Input data for the left cheek region.
+            input_data_right_cheek (np.ndarray): Input data for the right cheek region.
+            input_data_mouth (np.ndarray): Input data for the mouth region.
+            labels (np.ndarray): Corresponding labels for the input data.
+            number_of_epochs (int): Number of epochs to train.
+        """
+        checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath='trained_models/single_model.keras', monitor='val_loss',
+            verbose=1, save_best_only=True, mode='min')
+
+        dummy_inputs = {
+            'input_face': np.random.rand(3, 32, 80, 80, 7),
+            'input_forehead': np.random.rand(3, 32, 32, 32, 7),
+            'input_left_cheek': np.random.rand(3, 32, 24, 24, 7),
+            'input_right_cheek': np.random.rand(3, 32, 24, 24, 7),
+            'input_mouth': np.random.rand(3, 32, 20, 20, 7)
+        }
+        dummy_labels = np.array([[0, 1], [1, 0], [0, 1]])
+
+        self.model.fit(
+            dummy_inputs,  # Pass inputs as a dictionary
+            dummy_labels,
+            epochs=number_of_epochs,
+            batch_size=1,
+            shuffle=True
+        )
+
+
+        ### WIP !! FROM HERE !!
